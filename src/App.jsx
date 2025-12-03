d51cc595f0f7168b89d856b4e126813b404869f6
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-12-03 19:28:59 +0500

    Projectssetup:(sass introduction)

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import AppRouter from "./config/Router"
+
+
+function App() {
+
+
+  return (
+    
+      <div>
+
+          <AppRouter/>
+
+      </div>
+  
+  )
+}
+
+export default App
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import logo from "../../assets/logo.svg"
+import "./CSS/style.scss"
+
+function HomePage() {
+    return (
+        <div className="container">
+            <div className="header-bar">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div>
+                      <ul>
+                        </ul>  
+                </div>
+
+            </div>
+            
+        </div>
+    )
+    
+}
+
+export default HomePage;
