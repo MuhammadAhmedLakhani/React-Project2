dc3b1d37b9335b415fa6eac111c9c34a6951c49d
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-12-03 22:12:51 +0500

    AirforShare:UI V1

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,11 @@
 import logo from "../../assets/logo.svg"
-import "./CSS/style.scss"
+import "./CSS/style.scss";
+
+import TEXTCOLOR from '../../assets/text-color.svg';
+import TEXTGREY from '../../assets/text-grey.svg';
+import FILECOLOR from '../../assets/file-color.svg';
+import FILEGREY from '../../assets/file-grey.svg';
+
 
 function HomePage() {
     return (
@@ -9,15 +15,50 @@ function HomePage() {
                     <img src={logo} alt="" />
                 </div>
                 <div>
-                      <ul>
-                        </ul>  
+                    <ul className="menu-bar">
+
+                        <li>How it works</li>
+                        <li>Download</li>
+                        <li>Upgrade</li>
+                        <li>Feedback</li>
+                        <li className="menu-btn">Login / Register</li>
+                    </ul>
                 </div>
 
             </div>
-            
+            <div className="main-card">
+
+                <div className="card-sidebar">
+
+                    <div >
+                        <img src={TEXTCOLOR} alt="" />                        
+                    </div>
+                     
+                    <div >
+                        <img src={FILEGREY} alt="" />                        
+                    </div>
+                     
+
+                </div>
+
+                <div className="card-container">
+                    <div className="text-section">
+                        <h1>TEXT</h1>
+                    </div>
+                    <div className="file-section">
+                        <h1>FILES</h1>
+
+                    </div>
+
+                </div>
+
+
+
+
+            </div>
         </div>
     )
-    
+
 }
 
 export default HomePage;
